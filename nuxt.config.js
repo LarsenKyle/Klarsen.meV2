7c08a429e9906feaa546edb59f04f700093142cf
LarsenKyle <krlars87@gmail.com>
2021-02-12 18:08:04 -0500

    Nav, Her, About

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,12 +11,12 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.svg' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  
+
   css: [
   ],
 
